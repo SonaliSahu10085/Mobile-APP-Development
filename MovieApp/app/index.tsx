2740fb71cf17b366d07c6b75e91caba181a26295
Sonali Sahu <sonalisahu0064@gmail.com>
2025-08-29 15:15:29 +0530

    Setup Styling using Nativewind, Checkout the official website for nativewind

--- MovieApp/app/index.tsx
+++ MovieApp/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-blue-500">Welcome to My Movie App</Text>
     </View>
   );
 }
